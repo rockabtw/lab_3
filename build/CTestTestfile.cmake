499edc7d04318d47d6ad39351b3babcc91334fc1
Evgeniy Pavlov <evgeniirck@gmail.com>
2025-05-14 01:00:05 +0900

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/practica/LABA_3/lab_3
+# Build directory: C:/practica/LABA_3/lab_3/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
